a73347031fbb2b0940faf18388577de4ddde2321
MiroMuro <miro.laukkanen44@gmail.com>
2024-10-15 11:33:52 +0300

    Luigi can now jump on click

--- init.js
+++ init.js
@@ -1,4 +1,12 @@
 const init = () => {
-  const luigi = document.querySelector("#luigi");
+  const luigi = document.getElementById("luigi");
+  luigi.addEventListener("click", () => {
+    luigi.style.animation =
+      "luigi-jump 0.3s linear forwards, drive 5s linear infinite both";
+  });
+  luigi.addEventListener("animationend", () => {
+    luigi.style.animation = "drive 5s linear infinite both";
+  });
   console.log(luigi);
 };
+window.onload = init;
