94b0d20ea1706345148e99dfb1e227685cb05685
MiroMuro <miro.laukkanen44@gmail.com>
2024-10-21 23:35:19 +0300

    Added a crude slider for biography

--- init.js
+++ init.js
@@ -1,10 +1,12 @@
 const init = () => {
   const luigi = document.getElementById("luigi");
   luigi.addEventListener("click", () => {
+    console.log("Luigi clicked");
     luigi.style.animation =
-      "luigi-jump 0.3s linear forwards, drive 5s linear infinite both";
+      "luigi-jump 0.6s cubic-bezier(0.5, 0.05, 0.1, 1) forwards, drive 5s linear infinite both";
   });
   luigi.addEventListener("animationend", () => {
+    console.log("Luigi animation ended");
     luigi.style.animation = "drive 5s linear infinite both";
   });
   console.log(luigi);
