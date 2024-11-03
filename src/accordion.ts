89db27d56558544f6eae344210742c153fb30fa5
MiroMuro <miro.laukkanen44@gmail.com>
2024-11-03 19:50:43 +0200

    Added smooth accordion opening

--- src/accordion.ts
+++ src/accordion.ts
@@ -17,10 +17,10 @@ document.addEventListener("DOMContentLoaded", () => {
         Array.from(accordions).indexOf(accordionButton);
       closeOtherAccordionPanels(indexOfAccordionButton);
 
-      if (accordionPanel.style.display === "block") {
-        accordionPanel.style.display = "none";
+      if (accordionPanel.style.maxHeight) {
+        accordionPanel.style.maxHeight = "";
       } else {
-        accordionPanel.style.display = "block";
+        accordionPanel.style.maxHeight = accordionPanel.scrollHeight + "px";
       }
     });
   }
@@ -29,12 +29,14 @@ document.addEventListener("DOMContentLoaded", () => {
     for (let i = 0; i < accordions.length; i++) {
       if (i !== index) {
         const accordionButton = accordions[i] as HTMLElement;
+
         if (accordionButton.classList.contains("active")) {
           accordionButton.classList.remove("active");
         }
+
         const accordionPanel = accordions[i].nextElementSibling as HTMLElement;
-        if (accordionPanel.style.display === "block") {
-          accordionPanel.style.display = "none";
+        if (accordionPanel.style.maxHeight) {
+          accordionPanel.style.maxHeight = "";
         }
       }
     }
